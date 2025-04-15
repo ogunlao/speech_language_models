7e50df46f78ab1a6da662a4b3be1d024ed8ca07e
ogunlao <ogunlao@yahoo.com>
2025-04-15 02:43:04 +0200

    Add vq with multiple groups

--- model/quantizer.py
+++ model/quantizer.py
@@ -13,12 +13,12 @@ class VQ(nn.Module):
         self.codebook_size = codebook_size
         self.codebook_dim = codebook_dim
         self.num_groups = num_groups
-        if params.codebook_type == "simple":
+        if self.num_groups == 1:
             self.codebook = nn.Embedding(num_embeddings=self.codebook_size, 
                                         embedding_dim=self.codebook_dim)
-        elif params.codebook_type == "shared":
+        elif self.num_groups > 1:
             assert self.codebook_dim % self.num_groups == 0.
-            nn.codebooks = nn.ModuleList(
+            self.codebooks = nn.ModuleList(
                 nn.Embedding(num_embeddings=self.codebook_size, 
                         embedding_dim=self.codebook_dim//self.num_groups) for _ in range(self.num_groups)
             )
@@ -29,39 +29,78 @@ class VQ(nn.Module):
                 nn.Linear(params.feat_dim, params.feat_dim, bias=False,),
                 nn.Dropout(params.dropout_prob),
                 nn.ReLU(),
-                nn.Linear(params.feat_dim, self.codebook_size, bias=False,)
+                nn.Linear(params.feat_dim, self.codebook_size * self.num_groups, bias=False,)
             )
     
     def gumbel_estimator(self, x, annealing_weight):
-        logits = self.gumbel_proj(x.transpose(2, 1))
+        bs, channel, time = x.size()
+        logits = self.gumbel_proj(x.transpose(2, 1)) # bs, time, cb_size*num_grps
         
         # apply the gumbel softmax
         u = torch.rand(logits.size())
         v = -1*torch.log(-1*torch.log(u))
         
         logits = (logits + u)/annealing_weight
-        logits = F.softmax(logits, dim=2)
-
-        indexes = torch.argmax((logits + u)/annealing_weight, dim=2)
         
-        quantized = self.codebook(indexes)
-        quantized = quantized.transpose(2, 1)
+        if self.num_groups > 1:
+            logits = logits.reshape(-1, time, self.codebook_size, self.num_groups)
+
+            quantized, indexes = [], []
+            for group in range(self.num_groups):
+                group_logits = logits[:, :, :, group]
+                group_logits = F.softmax(group_logits, dim=2)
+                group_indexes = torch.argmax(group_logits, dim=2)
+                
+                group_quantized = self.codebooks[group](group_indexes) # batch, time, channels
+                quantized.append(group_quantized)
+                indexes.append(group_indexes)
+            quantized = torch.concat(quantized, dim=2)
+            quantized = quantized.transpose(2, 1)
+        else:
+            logits = F.softmax(logits, dim=2)
+            indexes = torch.argmax(logits, dim=2)
+            
+            quantized = self.codebook(indexes)
+            quantized = quantized.transpose(2, 1)
         
         return quantized, indexes
     
+    def find_closest_emb(self, x, emb):
+        emb = emb.unsqueeze(0).unsqueeze(1)
+        x = x.permute(0, 2, 1).unsqueeze(2)
+        dist = torch.sum((emb - x)**2, dim=-1)
+        
+        indexes = torch.argmin(dist, dim=2)
+        
+        return indexes
+        
     def kmeans_estimator(self, x):
         # x: batch, channels, time
         
-        codes = self.codebook.weight # num_code, channels
-        
-        # find nearest cluster for each 
-        codes = codes.unsqueeze(0).unsqueeze(1)
-        x = x.permute(0, 2, 1).unsqueeze(2)
-        dist = torch.sum((codes - x)**2, dim=-1)
-        
-        indexes = torch.argmin(dist, dim=2)
-        quantized = self.codebook(indexes)
-        quantized = quantized.transpose(2, 1)
+        if self.num_groups > 1:
+            batch, channels, time = x.size()
+            x = x.view(batch, self.codebook_dim//self.num_groups, self.num_groups, time)
+            quantized, indexes = [], []
+            for group, codebook in enumerate(self.codebooks):
+                x_group = x[:, :, group, :].squeeze(2) 
+                
+                # find nearest cluster for each
+                group_indexes = self.find_closest_emb(x_group, codebook.weight)
+                group_quantized = codebook(group_indexes)
+                
+                quantized.append(group_quantized)
+                indexes.append(group_indexes)
+                
+            # combine retrieved quantized vectors from different groups
+            quantized = torch.concat(quantized, dim=2)
+            quantized = quantized.transpose(2, 1)
+        else:
+            # find nearest cluster for each 
+            indexes = self.find_closest_emb(x, self.codebook.weight)
+            
+            # retrieve quantized embeddings
+            quantized = self.codebook(indexes)
+            quantized = quantized.transpose(2, 1)
         
         return quantized, indexes
     
